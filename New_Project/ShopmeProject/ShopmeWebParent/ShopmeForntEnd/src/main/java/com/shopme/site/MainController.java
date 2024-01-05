0f7543e3541aa9c4643e24dbaa27090b8b7863f7
Rashmi Ranjan Mohapatra <rashmilipu1980@outlook.com>
2024-01-05 18:21:07 +0530

    shop me front end  added

--- New_Project/ShopmeProject/ShopmeWebParent/ShopmeForntEnd/src/main/java/com/shopme/site/MainController.java
+++ New_Project/ShopmeProject/ShopmeWebParent/ShopmeForntEnd/src/main/java/com/shopme/site/MainController.java
@@ -1,14 +1,14 @@
-package com.shopme.admin;
+package com.shopme.site;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class MainController {
 	@GetMapping("")
-	public String viewHomepage() {
+	public String viewHomePage() {
 		return "index";
 	}
-	
 
 }
